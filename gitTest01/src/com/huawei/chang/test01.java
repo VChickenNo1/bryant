89e5aa67afb8b33b8fef0d986aaa0ce9e832e139
root <15929902691@163.com>
2020-03-11 22:02:49 +0800

    ceshi

--- gitTest01/src/com/huawei/chang/test01.java
+++ gitTest01/src/com/huawei/chang/test01.java
@@ -1,10 +1,23 @@
 package com.huawei.chang;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Create by Administrator on 2020/3/11
  */
 public class test01 {
     public static void main(String[] args) {
         System.out.println("hello,world!");
+        System.out.println(method01());
+    }
+
+    private static String method01() {
+        Map<String, String> map = new HashMap<>();
+        map.put("zhangsan", "nan");
+        map.put("lisi", "nv");
+        map.put("wangwu", "nan");
+        return map.toString();
+
     }
 }
