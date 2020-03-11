bc12a91f5284629d91f61205ec2e501f7d9c62a7
root <15929902691@163.com>
2020-03-11 21:24:58 +0800

    添加任务

---
+++ gitTest01/src/com/huawei/chang/test01.java
@@ -0,0 +1,10 @@
+package com.huawei.chang;
+
+/**
+ * Create by Administrator on 2020/3/11
+ */
+public class test01 {
+    public static void main(String[] args) {
+        System.out.println("hello,world!");
+    }
+}
